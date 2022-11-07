7074c96882369c5cb4a1aa3298be967a7aecea62
Ekaterina Kovaleva <days@mail.ru>
2022-11-08 03:06:43 +0500

    решение задачи 3

--- C#_1/C#_1_3/Program.cs
+++ C#_1/C#_1_3/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int numberA =-3;
+if (numberA%2==0)
+{
+  Console.Write ("Число является четным");
+}
+if (numberA%2!=0)
+{
+    Console.Write ("Число является нечетным");
+}
+
