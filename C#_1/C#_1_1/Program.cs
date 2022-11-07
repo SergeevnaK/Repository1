ba236c8b326f2a0c2027fc9e7fc03260c1ab7434
Ekaterina Kovaleva <days@mail.ru>
2022-11-08 01:46:09 +0500

    решение задачи 1

--- C#_1/C#_1_1/Program.cs
+++ C#_1/C#_1_1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("111 Hello, World!");
+﻿int numberA =-3;
+int numberB =-6;
+int max =1;
+int min =0;
+if (numberA>numberB)
+{
+  max =numberA;
+  min =numberB;
+}
+else 
+{
+   max =numberB;
+   min =numberA;
+}
+Console.Write ("Большее число: ");
+Console.WriteLine(max);
+Console.Write ("Меньшее число: ");
+Console.WriteLine(min);
