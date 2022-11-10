a1baa01a292dd3f83820a721f6c30e440f2070ee
Ekaterina Kovaleva <days@mail.ru>
2022-11-10 22:34:11 +0500

    решение задачи 15

--- C#_2/C#_2_3/Program.cs
+++ C#_2/C#_2_3/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+int number = ReadInt("Введите число: ");
+int count = number.ToString().Length;
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+    if (number >7) 
+    {
+        Console.Write ("Число не обозначает день недели");
+    }
+    else
+    if (number >5) 
+    {
+        Console.Write ("Число обозначает выходной день недели");
+    }
+    else 
+    Console.Write ("Число обозначает рабочий день недели");
+    
