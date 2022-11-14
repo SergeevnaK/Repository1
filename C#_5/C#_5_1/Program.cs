416369f9be3c66f4995970cc54018ca77b514074
Ekaterina Kovaleva <days@mail.ru>
2022-11-14 22:18:45 +0500

    решение задачи 34

--- C#_5/C#_5_1/Program.cs
+++ C#_5/C#_5_1/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+void FillMass(int[] mass)
+{
+    for(int i=0; i<mass.Length; i++) mass[i] = new Random().Next(100,1000);
+}
+void PrintMass(int[] mass)
+{
+    for(int i=0; i<mass.Length; i++) Console.Write($"{mass[i]} ");
+    Console.WriteLine();
+}
+void Schetchik(int[] mass)
+{
+    int chek=0;
+    for(int i=0; i<mass.Length; i++) 
+    {
+       if (mass[i]%2 ==0) chek++;
+    }
+    Console.Write($"Количество четных чисел: {chek}");
+}
+Console.Write("Количество чисел для вывода: " );
+int a=Convert.ToInt32(Console.ReadLine());
+int[] array = new int[a];
+FillMass(array);
+PrintMass(array);
+Schetchik(array);
