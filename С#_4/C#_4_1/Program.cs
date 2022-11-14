2c4e9fc73ad2f15e434f8cbfe39fbd768080555f
Ekaterina Kovaleva <days@mail.ru>
2022-11-14 21:08:45 +0500

    решение задачи 25

--- С#_4/C#_4_1/Program.cs
+++ С#_4/C#_4_1/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 25: Напишите метод, который принимает на вход два числа (A и B) и возводит число A в целую степень B.
+
+Console.Write("Введите число, которое хотите возвести в степень: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число равное степени: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int c=1;
+for (int i = 0; i < b; i++) c*=a;
+Console.Write("Число {0} в степени {1} равно {2}", a, b, c);
