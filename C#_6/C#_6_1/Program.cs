7289c36394899feb2e3db286d32f29a7f34417fd
Ekaterina Kovaleva <days@mail.ru>
2022-11-16 00:31:08 +0500

    решение задачи 41

--- C#_6/C#_6_1/Program.cs
+++ C#_6/C#_6_1/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Write("Введите элементы(через пробел): ");
+int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse); 
+int chek = 0;
+ 
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        chek++;
+    }
+}
+ 
+Console.WriteLine($"Кол-во элементов больше 0: {chek}");
