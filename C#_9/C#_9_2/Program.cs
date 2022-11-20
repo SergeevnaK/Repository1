bcb772d46d595e170e04bbf23c938dd19c3485d4
Ekaterina Kovaleva <days@mail.ru>
2022-11-20 16:24:07 +0500

    решение задачи 66

--- C#_9/C#_9_2/Program.cs
+++ C#_9/C#_9_2/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 66: Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите натуральное число от которого считать:");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите натуральное число до которого считать:");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int SumNaturElem (int m, int n, int sum)
+{
+    if (m > n) 
+    { 
+        return sum;
+    }
+    sum = sum + (m++);
+    return SumNaturElem(m, n, sum);
+}
+
+Console.WriteLine($"Сумма от натуральных чисел от {M} до {N} равна: {SumNaturElem(M, N, 0)}");
